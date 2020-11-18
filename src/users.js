5b3f3591d3cea7f199ae6dcbecb54b125bc0b54c
pskalbhav1 <praseedhakalbhavi@gmail.com>
2020-11-18 10:40:30 +0530

    Eval

---
+++ src/users.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Show, SimpleShowLayout, TextField,UrlField } from 'react-admin';
+
+export const UserList = props => (
+    <Show {...props}>
+        <SimpleShowLayout rowClick="show">
+            <TextField source="id" />
+            <TextField source="student" />
+            <UrlField source="reasoning_ans_link" />
+            <TextField source="reasoning_marks" />
+            <UrlField source="cs_ans_link" />
+            <TextField source="cs_ans_marks" />
+            <UrlField source="elec_ans_link" />
+            <TextField source="elec_ans_marks" />
+            <TextField source="set_no" />
+        </SimpleShowLayout>
+    </Show>
+);
